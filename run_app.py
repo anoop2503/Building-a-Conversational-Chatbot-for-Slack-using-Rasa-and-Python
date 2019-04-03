81fa19e26b0b1f9c2d14b9d41da96b96adf68ba6
Anoop Mohan <48883637+anoop2503@users.noreply.github.com>
2019-04-03 15:16:04 +0530

    Updated run_app.py
    - Import os module
    - Using environment variable for accepting SLACK_OAUTH_ACCESS_TOKEN

--- run_app.py
+++ run_app.py
@@ -3,13 +3,13 @@ from rasa_core.agent import Agent
 from rasa_core.interpreter import RasaNLUInterpreter
 import yaml
 from rasa_core.utils import EndpointConfig
+import os
 
 
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/current')
 action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter, action_endpoint = action_endpoint)
 
-input_channel = SlackInput('xoxb-514185865477-514638817877-ZlAUhYSuoydYHkl0oCrUU7MC' #your bot user authentication token
-                           )
+input_channel = SlackInput(os.environ.get('SLACK_OAUTH_ACCESS_TOKEN'))
 
 agent.handle_channels([input_channel], 5004, serve_forever=True)
